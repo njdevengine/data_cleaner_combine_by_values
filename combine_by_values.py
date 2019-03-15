af51567d8ffb757f3fac744773efbda75b9d3711
njdevengine <newjerseydevelopment@gmail.com>
2019-03-15 16:34:25 -0400

    Create combine_by_values.py
    Takes all spreadsheets in a directory, identifies columns with certain values, renames those headers to a standard name, in this case status, and combines all files on this new column. Can be inserted into NLTK to extract data from combined text file.

---
+++ combine_by_values.py
@@ -0,0 +1,56 @@
+import glob
+import pandas as pd
+path = r'C:/file/path' # use your path
+all_files = glob.glob(path + "/*.xlsx")
+
+li = []
+
+for filename in all_files:
+    df = pd.read_excel(filename, index_col=None, header=0)
+#     df['file'] = filename
+#     print(filename)
+    li.append(df)
+
+headers = []
+length = len(li)-1
+for i in range(0,length):
+    column_names = list(li[i].head(0))
+    headers.append(column_names)
+
+taken = []
+for i in range(0,length):
+    taken.append(li[i][li[i].apply(lambda r: r.str.contains('Taken', case=False).any(), axis=1)])
+
+open = []
+for i in range(0,length):
+    open.append(li[i][li[i].apply(lambda r: r.str.contains('Open', case=False).any(), axis=1)])   
+
+data = []
+length_headers = len(headers)-1
+for n in range(0,length_headers):
+    if any("Unnamed" in s for s in headers[n]):
+        data.append(n)
+
+nums = []
+headers = []
+for i in range (0,length):
+    try:
+        headers.append(li[i].describe(include="all").transpose().index[li[i].describe(include="all").transpose().top =="Taken"][0])
+        nums.append(i)
+    except IndexError:
+        headers.append(li[i].describe(include="all").transpose().index[li[i].describe(include ="all").transpose().top =="Open"][0])
+        nums.append(i)
+dict = {}
+for i in range(len(nums)):
+    dict[nums[i]] = headers[i]
+
+length2 = len(open)-2
+for i in range (0,length2):
+    open[i].columns = open[i].columns.str.replace(dict[i], 'status')
+
+for i in range(0,length2):
+    print(i)
+    open[0] = pd.concat([open[0],open[i+1]], sort = False)
+
+open[0].to_csv('full_list.csv')
+open[0].to_csv('text_list.txt', sep='\t', index=False)
