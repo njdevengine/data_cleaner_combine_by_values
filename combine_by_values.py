fe51195471f8f0b3d4e0fee3343fbf1291e12ceb
njdevengine <newjerseydevelopment@gmail.com>
2019-03-16 10:23:31 -0400

    Update combine_by_values.py

--- combine_by_values.py
+++ combine_by_values.py
@@ -7,23 +7,21 @@ li = []
 
 for filename in all_files:
     df = pd.read_excel(filename, index_col=None, header=0)
+#     for troubleshooting file errors, can enable below    
 #     df['file'] = filename
 #     print(filename)
     li.append(df)
-
+ 
 headers = []
+taken_filter = []
+open_filter = []
+
 length = len(li)-1
 for i in range(0,length):
     column_names = list(li[i].head(0))
     headers.append(column_names)
-
-taken = []
-for i in range(0,length):
-    taken.append(li[i][li[i].apply(lambda r: r.str.contains('Taken', case=False).any(), axis=1)])
-
-open = []
-for i in range(0,length):
-    open.append(li[i][li[i].apply(lambda r: r.str.contains('Open', case=False).any(), axis=1)])   
+    taken_filter.append(li[i][li[i].apply(lambda r: r.str.contains('Taken', case=False).any(), axis=1)])
+    open_filter.append(li[i][li[i].apply(lambda r: r.str.contains('Open', case=False).any(), axis=1)])   
 
 data = []
 length_headers = len(headers)-1
@@ -40,17 +38,19 @@ for i in range (0,length):
     except IndexError:
         headers.append(li[i].describe(include="all").transpose().index[li[i].describe(include ="all").transpose().top =="Open"][0])
         nums.append(i)
+        
 dict = {}
 for i in range(len(nums)):
     dict[nums[i]] = headers[i]
 
-length2 = len(open)-2
+length2 = len(open)-1
 for i in range (0,length2):
     open[i].columns = open[i].columns.str.replace(dict[i], 'status')
 
 for i in range(0,length2):
     print(i)
     open[0] = pd.concat([open[0],open[i+1]], sort = False)
-
+    
+#may want to remove headers for text output
 open[0].to_csv('full_list.csv')
 open[0].to_csv('text_list.txt', sep='\t', index=False)
